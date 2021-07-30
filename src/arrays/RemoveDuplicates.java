b9b855f01d7e51c6de9a69b22c27ad34c10409d3
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-07-31 00:55:49 +0600

    Initial commit

---
+++ src/arrays/RemoveDuplicates.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class RemoveDuplicates {
+
+    public static int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(RemoveDuplicates.removeDuplicates(new int[]{1,2,2,3,3}));
+    }
+}
