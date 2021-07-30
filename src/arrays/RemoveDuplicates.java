46a31582b99fe42e746b85cabc9a06abaa828a97
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2021-07-31 00:58:27 +0600

    RemoveDuplicates From Array

--- src/arrays/RemoveDuplicates.java
+++ src/arrays/RemoveDuplicates.java
@@ -18,6 +18,7 @@ package arrays;
  * Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
  * Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
  * It does not matter what you leave beyond the returned k (hence they are underscores).
+ * **
  * */
 
 public class RemoveDuplicates {
